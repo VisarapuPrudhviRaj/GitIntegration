c7f9cbc8b9914fc9c4f0f101019337ab989872dc
VisarapuPrudhviRaj <prudhvirajvisarapu@gmail.com>
2019-03-08 15:12:39 +0530

    List Activity

--- app/src/main/java/pr/bluefrog/myapplication/ListActivity.java
+++ app/src/main/java/pr/bluefrog/myapplication/ListActivity.java
@@ -3,12 +3,11 @@ package pr.bluefrog.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /*fsdklfj */
+        setContentView(R.layout.activity_list);
     }
 }
