d95d265900365a66ae96b8074f57a1948c96731b
VisarapuPrudhviRaj <prudhvirajvisarapu@gmail.com>
2019-03-14 11:32:52 +0530

    Gradle

--- app/src/main/java/pr/bluefrog/myapplication/MainActivity.java
+++ app/src/main/java/pr/bluefrog/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package pr.bluefrog.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*fsdklfj */
+
+
+
+    }
+
+    public void onClick_List(View view) {
+        startActivity(new Intent(MainActivity.this, ListActivity.class));
     }
 }
